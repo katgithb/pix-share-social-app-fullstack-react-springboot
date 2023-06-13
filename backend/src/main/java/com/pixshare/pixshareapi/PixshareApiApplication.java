96554043b685e8342c7a804d0b4b7a122017d175
katgithb <akcfun9@gmail.com>
2023-06-13 16:05:20 +0545

    Initial commit - Scaffolding frontend and backend skeleton

---
+++ backend/src/main/java/com/pixshare/pixshareapi/PixshareApiApplication.java
@@ -0,0 +1,13 @@
+package com.pixshare.pixshareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixshareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixshareApiApplication.class, args);
+	}
+
+}
