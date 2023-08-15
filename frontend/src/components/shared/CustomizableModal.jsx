2efaf1cafc7a0ede908295f0e6f17a5cdc4c0ae0
katgithb <akcfun9@gmail.com>
2023-08-15 20:00:45 +0545

    Initial commit - shared react components

---
+++ frontend/src/components/shared/CustomizableModal.jsx
@@ -0,0 +1,42 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import React from "react";
+
+const CustomizableModal = ({
+  isOpen,
+  onClose,
+  size,
+  header,
+  footer,
+  showModalCloseButton,
+  isCentered = true,
+  scrollBehavior,
+  children,
+}) => {
+  return (
+    <Modal
+      size={size}
+      isOpen={isOpen}
+      onClose={onClose}
+      scrollBehavior={scrollBehavior}
+      isCentered={isCentered}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        {header && <ModalHeader>{header}</ModalHeader>}
+        {showModalCloseButton && <ModalCloseButton mr={1} />}
+        <ModalBody>{children}</ModalBody>
+        {footer && <ModalFooter>{footer}</ModalFooter>}
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CustomizableModal;
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,629 @@
+import altLogo from "../../assets/images/pixshare_logo_gray.png";
+import logo from "../../assets/images/pixshare_logo.png";
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Button,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Stack,
+  Icon,
+  IconButton,
+  useDisclosure,
+  useColorModeValue,
+  Image,
+  VStack,
+  MenuDivider,
+  useColorMode,
+  Input,
+  Link,
+  Center,
+  chakra,
+  InputGroup,
+  InputLeftElement,
+  Collapse,
+  Fade,
+  ScaleFade,
+} from "@chakra-ui/react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai";
+import { FiMenu, FiMoon, FiSun, FiX } from "react-icons/fi";
+import {
+  MdExpandCircleDown,
+  MdOutlineExpandCircleDown,
+  MdOutlineSearch,
+} from "react-icons/md";
+import {
+  FaBookmark,
+  FaGear,
+  FaMagnifyingGlass,
+  FaRegCommentDots,
+  FaRegCompass,
+  FaRegHeart,
+  FaRegSquarePlus,
+  FaRightFromBracket,
+  FaUser,
+} from "react-icons/fa6";
+import { PiHouseBold } from "react-icons/pi";
+import { Link as RouteLink, NavLink, useLocation } from "react-router-dom";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import CreatePostModal from "../post/CreatePostModal/CreatePostModal";
+import { useEffect, useState } from "react";
+import { TbLayoutNavbarCollapse, TbLayoutNavbarExpand } from "react-icons/tb";
+import SearchResultsListModal from "../search/SearchResultsListModal/SearchResultsListModal";
+import { BiSearch, BiSearchAlt } from "react-icons/bi";
+import { ImSearch } from "react-icons/im";
+import { GrSearch } from "react-icons/gr";
+import { BsSearch } from "react-icons/bs";
+import SearchInputBar from "../search/SearchInputBar";
+
+const menuLinks = [
+  {
+    name: "Profile",
+    path: "/username",
+    icon: FaUser,
+  },
+  {
+    name: "Saved",
+    path: "/",
+    icon: FaBookmark,
+  },
+  {
+    name: "Settings",
+    path: "/account/settings",
+    icon: FaGear,
+  },
+];
+
+const Navbar = () => {
+  // const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
+  const {
+    isOpen: isOpenNavModal,
+    onOpen: onOpenNavModal,
+    onClose: onCloseNavModal,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenNewPostModal,
+    onOpen: onOpenNewPostModal,
+    onClose: onCloseNewPostModal,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenSearchResultsListModal,
+    onOpen: onOpenSearchResultsListModal,
+    onClose: onCloseSearchResultsListModal,
+  } = useDisclosure();
+  const [isNavbarCollapsed, setIsNavbarCollapsed] = useState(false);
+  const [showNavBar, setShowNavBar] = useState(false);
+  const [hideNavBar, setHideNavBar] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [isButtonClickPending, setIsButtonClickPending] = useState(false);
+
+  const navLinks = [
+    {
+      name: "Home",
+      path: "/",
+      isLinkEmpty: false,
+      icon: <PiHouseBold />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+    {
+      name: "Comments",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegCommentDots />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+    {
+      name: "Create Post",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegSquarePlus />,
+      handleNavLinkClick: onOpenNewPostModal,
+    },
+    {
+      name: "Discover",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegCompass />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+    {
+      name: "Likes",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegHeart />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+  ];
+
+  const mobileNavLinks = [
+    {
+      name: "Home",
+      path: "/",
+      isLinkEmpty: false,
+      icon: <PiHouseBold />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+    {
+      name: "Search",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <GrSearch />,
+      handleNavLinkClick: onOpenSearchResultsListModal,
+    },
+    {
+      name: "Create Post",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegSquarePlus />,
+      handleNavLinkClick: onOpenNewPostModal,
+    },
+    {
+      name: "Discover",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegCompass />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+    {
+      name: "Likes",
+      path: "#",
+      isLinkEmpty: true,
+      icon: <FaRegHeart />,
+      handleNavLinkClick: onCloseNavModal,
+    },
+  ];
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const isScrolledToTop = window.scrollY === 0;
+  //     const isScrollingDown = window.scrollY > 0;
+
+  //     if (isScrolledToTop && !isScrolling) {
+  //       setIsNavbarCollapsed(false);
+  //       setHideNavBar(false);
+  //     } else {
+  //       setIsNavbarCollapsed(true);
+  //       setHideNavBar(true);
+  //     }
+
+  //     //   setIsNavbarCollapsed(true);
+  //     //   setHideNavBar(true);
+  //     // } else {
+  //     //   setIsNavbarCollapsed(false);
+  //     //   setHideNavBar(false);
+  //     // }
+  //     // setIsNavbarCollapsed(isScrollingDown);
+  //   };
+
+  //   window.addEventListener("scroll", handleScroll);
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, [isScrolling]);
+
+  // const handleToggleNav = () => {
+  //   setHideNavBar(!hideNavBar);
+  // };
+
+  // const handleToggleNav = () => {
+  //   setIsButtonClickPending(true);
+  //   setTimeout(() => {
+  //     setHideNavBar((prevHideNavBar) => !prevHideNavBar);
+  //     setIsButtonClickPending(false);
+  //   }, 200); // Adjust the delay as needed
+  // };
+
+  const handleScroll = () => {
+    const isScrollingDown = window.scrollY > 0;
+
+    if (isScrollingDown) {
+      setIsNavbarCollapsed(true);
+    } else {
+      setIsNavbarCollapsed(false);
+      setShowNavBar(false);
+    }
+  };
+
+  const handleToggleNav = () => {
+    if (window.scrollY > 0) {
+      setShowNavBar(!showNavBar);
+      onCloseNavModal();
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  return (
+    <>
+      <Box
+        as="header"
+        px={4}
+        boxShadow="lg"
+        bg={useColorModeValue("gray.100", "gray.700")}
+        width="100%"
+        position="sticky"
+        top="0"
+        zIndex="sticky"
+      >
+        <CreatePostModal
+          isOpen={isOpenNewPostModal}
+          onClose={onCloseNewPostModal}
+        />
+
+        <Collapse
+          startingHeight={20}
+          in={!isNavbarCollapsed || showNavBar}
+          animateOpacity
+        >
+          <Flex
+            h={16}
+            alignItems="center"
+            justifyContent="space-between"
+            // maxW={1280}
+            mx="auto"
+            pointerEvents={!isNavbarCollapsed || showNavBar ? "auto" : "none"}
+            opacity={!isNavbarCollapsed || showNavBar ? 1 : 0}
+            transition="opacity 0.3s ease"
+          >
+            <IconButton
+              size="md"
+              icon={isOpenNavModal ? <AiOutlineClose /> : <GiHamburgerMenu />}
+              aria-label="Open Menu"
+              display={["inherit", "inherit", "none"]}
+              onClick={isOpenNavModal ? onCloseNavModal : onOpenNavModal}
+            />
+            <HStack spacing={8} alignItems="center">
+              <Link as={RouteLink} to="/">
+                <Image
+                  alt="Logo"
+                  w={"auto"}
+                  h={10}
+                  src={useColorModeValue(logo, altLogo)}
+                />
+              </Link>
+              <HStack
+                display={{ base: "none", md: "flex" }}
+                alignItems="center"
+                justify={"center"}
+              >
+                <SearchResultsListModal
+                  isOpen={isOpenSearchResultsListModal}
+                  onClose={onCloseSearchResultsListModal}
+                />
+
+                <SearchInputBar
+                  isNavSearchBar={true}
+                  onOpenSearchModal={onOpenSearchResultsListModal}
+                />
+              </HStack>
+            </HStack>
+
+            <HStack alignItems={"center"}>
+              <HStack as="nav" display={{ base: "none", md: "block" }}>
+                {navLinks.map((link, index) => (
+                  <NonMobileNavLink
+                    key={index}
+                    {...link}
+                    onClose={onCloseNavModal}
+                  />
+                ))}
+                <IconButton
+                  onClick={toggleColorMode}
+                  variant={"ghost"}
+                  aria-label="Color Switcher"
+                  icon={colorMode === "light" ? <FiMoon /> : <FiSun />}
+                  _hover={{
+                    bg: useColorModeValue("gray.200", "gray.600"),
+                    color: useColorModeValue("blue.500", "blue.200"),
+                  }}
+                  fontSize="1.5rem"
+                />
+              </HStack>
+
+              <Menu isLazy>
+                <MenuButton as={Button} size="sm" px={0} py={0} rounded="full">
+                  <Avatar
+                    size="sm"
+                    src={
+                      "https://api.dicebear.com/6.x/adventurer-neutral/svg?seed=Sam&backgroundColor=c0aede"
+                    }
+                  />
+                </MenuButton>
+                <MenuList
+                  zIndex={5}
+                  border="2px solid"
+                  borderColor={useColorModeValue("gray.700", "gray.100")}
+                  // boxShadow="4px 4px 0"
+                >
+                  <Link
+                    as={RouteLink}
+                    to="/username"
+                    _hover={{ textDecoration: "none" }}
+                  >
+                    <MenuItem justifyContent={"center"} alignItems={"center"}>
+                      <VStack>
+                        <Avatar
+                          size="xl"
+                          src={
+                            "https://api.dicebear.com/6.x/adventurer-neutral/svg?seed=Sam&backgroundColor=c0aede"
+                          }
+                        />
+                        <Text size="sm" color="gray.400" mt="0 !important">
+                          @darren_criss
+                        </Text>
+                        <Text fontWeight="500">Darren Criss</Text>
+                      </VStack>
+                    </MenuItem>
+                  </Link>
+                  <MenuDivider />
+                  {menuLinks.map((link, index) => (
+                    <MenuLink key={index} {...link} onClose={onCloseNavModal} />
+                  ))}
+                  <MenuDivider />
+                  <MenuItem
+                    onClick={toggleColorMode}
+                    _hover={{
+                      color: "blue.400",
+                      bg: useColorModeValue("gray.200", "gray.600"),
+                    }}
+                    display={{ base: "block", md: "none" }}
+                  >
+                    <HStack>
+                      <Icon
+                        as={colorMode === "light" ? MoonIcon : SunIcon}
+                        size={17}
+                      />
+                      <Text>
+                        {colorMode === "light" ? "Dark Mode" : "Light Mode"}
+                      </Text>
+                    </HStack>
+                  </MenuItem>
+                  <Link
+                    as={RouteLink}
+                    to="/"
+                    _hover={{
+                      textDecoration: "none",
+                    }}
+                    onClick={() => onCloseNavModal()}
+                  >
+                    <MenuItem
+                      _hover={{
+                        color: "blue.400",
+                        bg: useColorModeValue("gray.200", "gray.600"),
+                      }}
+                    >
+                      <HStack>
+                        <Icon as={FaRightFromBracket} size={17} />
+                        <Text>Sign Out</Text>
+                      </HStack>
+                    </MenuItem>
+                  </Link>
+                </MenuList>
+              </Menu>
+            </HStack>
+          </Flex>
+        </Collapse>
+
+        {/* Mobile Screen Links */}
+        {isOpenNavModal ? (
+          <Box
+            pb={4}
+            display={
+              !isNavbarCollapsed || showNavBar
+                ? ["inherit", "inherit", "none"]
+                : "none"
+            }
+          >
+            <Stack as="nav" spacing={2}>
+              {mobileNavLinks.map((link, index) => (
+                <MobileNavLink
+                  key={index}
+                  {...link}
+                  onClose={onCloseNavModal}
+                />
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <IconButton
+        size="md"
+        icon={
+          !isNavbarCollapsed || showNavBar ? (
+            <TbLayoutNavbarCollapse />
+          ) : (
+            <TbLayoutNavbarExpand />
+          )
+        }
+        aria-label="Toggle Menu"
+        bg={"transparent"}
+        fontSize="24"
+        rounded="full"
+        boxShadow="md"
+        color={useColorModeValue("blue.400", "blue.500")}
+        position="fixed"
+        left="50%"
+        transform={
+          !isNavbarCollapsed || showNavBar
+            ? "translateX(-50%)"
+            : "translateX(-50%) translateY(-110%)"
+        }
+        top="45"
+        zIndex="sticky"
+        pointerEvents={window.scrollY > 0 ? "auto" : "none"}
+        opacity={window.scrollY > 0 ? 1 : 0}
+        transition="all 0.25s ease"
+        onClick={handleToggleNav}
+      />
+    </>
+  );
+};
+
+const NonMobileNavLink = ({
+  path,
+  isLinkEmpty,
+  icon,
+  handleNavLinkClick,
+  onClose,
+}) => {
+  const navLocation = useLocation();
+  const isActiveLink = navLocation.pathname === path;
+
+  const link = {
+    bg: useColorModeValue("gray.200", "gray.600"),
+    color: useColorModeValue("blue.500", "blue.200"),
+  };
+
+  const activeLink = {
+    bg: useColorModeValue("blue.400", "blue.500"),
+    color: useColorModeValue("gray.100", "gray.200"),
+  };
+
+  const hoverActiveLink = {
+    bg: useColorModeValue("blue.500", "blue.600"),
+    color: useColorModeValue("gray.200", "gray.300"),
+  };
+
+  return (
+    <Link
+      as={isLinkEmpty ? Link : NavLink}
+      to={path}
+      onClick={handleNavLinkClick}
+    >
+      {isActiveLink ? (
+        <IconButton
+          variant={"ghost"}
+          icon={icon}
+          color={activeLink.color}
+          bg={activeLink.bg}
+          _hover={{
+            bg: hoverActiveLink.bg,
+            color: hoverActiveLink.color,
+          }}
+          fontSize={"1.5rem"}
+        />
+      ) : (
+        <IconButton
+          variant={"ghost"}
+          icon={icon}
+          _hover={{
+            bg: link.bg,
+            color: link.color,
+          }}
+          fontSize={"1.5rem"}
+        />
+      )}
+    </Link>
+  );
+};
+
+const MobileNavLink = ({
+  name,
+  path,
+  isLinkEmpty,
+  icon,
+  handleNavLinkClick,
+  onClose,
+}) => {
+  const navLocation = useLocation();
+  const isActiveLink = navLocation.pathname === path;
+
+  const link = {
+    bg: useColorModeValue("gray.200", "gray.600"),
+    color: useColorModeValue("blue.500", "blue.200"),
+  };
+
+  const activeLink = {
+    bg: useColorModeValue("blue.400", "blue.500"),
+    color: useColorModeValue("gray.100", "gray.200"),
+  };
+
+  const hoverActiveLink = {
+    bg: useColorModeValue("blue.500", "blue.600"),
+    color: useColorModeValue("gray.200", "gray.300"),
+  };
+
+  const linkText = {
+    color: useColorModeValue("blue.500", "blue.200"),
+  };
+
+  return (
+    <Link
+      as={isLinkEmpty ? Link : NavLink}
+      to={path}
+      px={3}
+      py={1}
+      lineHeight="inherit"
+      rounded="md"
+      _hover={{
+        textDecoration: "none",
+        bg: link.bg,
+        color: link.color,
+      }}
+      onClick={handleNavLinkClick}
+    >
+      <HStack role="group">
+        {isActiveLink ? (
+          <IconButton
+            variant={"ghost"}
+            icon={icon}
+            color={activeLink.color}
+            bg={activeLink.bg}
+            _hover={{
+              bg: hoverActiveLink.bg,
+              color: hoverActiveLink.color,
+            }}
+            fontSize={"1.2rem"}
+          />
+        ) : (
+          <IconButton
+            variant={"ghost"}
+            icon={icon}
+            _hover={{
+              bg: link.bg,
+              color: link.color,
+            }}
+            fontSize={"1.2rem"}
+          />
+        )}
+        <Text color={isActiveLink ? linkText.color : ""}>{name}</Text>
+      </HStack>
+    </Link>
+  );
+};
+
+const MenuLink = ({ name, path, icon, onClose }) => {
+  return (
+    <Link
+      as={RouteLink}
+      to={path}
+      _hover={{
+        textDecoration: "none",
+      }}
+      onClick={() => onClose()}
+    >
+      <MenuItem
+        _hover={{
+          color: "blue.400",
+          bg: useColorModeValue("gray.200", "gray.600"),
+        }}
+      >
+        <HStack>
+          <Icon as={icon} size={17} />
+          <Text>{name}</Text>
+        </HStack>
+      </MenuItem>
+    </Link>
+  );
+};
+
+export default Navbar;
