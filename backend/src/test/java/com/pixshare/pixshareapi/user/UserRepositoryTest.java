bc821021e0ad6e8879ec9e3fe4b727199bab9c5a
katgithb <akcfun9@gmail.com>
2024-03-18 14:57:54 +0545

    Enable Elastic Beanstalk deployment (pixshare-api)
    - Introduce AWS Elastic Beanstalk configuration file for ECS managed Docker container for pixshare-api with environment variables.
    - Integrate pixshare-api service for backend to Docker Compose for local development.
    - Update Maven plugins configuration for Spring Boot build and deployment (Configure Jib plugin for container image building and update Spring Boot dependencies).
    - Enhance Spring profile activation based on environment variable (default: 'dev').
    - Improve test data format for user in UserRepositoryTest.

--- backend/src/test/java/com/pixshare/pixshareapi/user/UserRepositoryTest.java
+++ backend/src/test/java/com/pixshare/pixshareapi/user/UserRepositoryTest.java
@@ -34,7 +34,7 @@ class UserRepositoryTest extends AbstractTestcontainers {
 
         String firstName = FAKER.name().firstName();
         String lastName = FAKER.name().lastName();
-        username = "u123_" + firstName.toLowerCase() + "_" + lastName.toLowerCase();
+        username = "u12345_" + FAKER.name().username();
         name = firstName + " " + lastName;
         email = FAKER.internet().safeEmailAddress() + "-" + UUID.randomUUID();
         password = "password123#";
