afe9ddc86d67e5f1da70cb40a4f0e3d3052eb589
katgithb <akcfun9@gmail.com>
2024-04-05 14:28:06 +0545

    Refactor AWS EB config & add Spring Boot Actuator
    - Refactor AWS Elastic Beanstalk configuration to remove ECS container for pixshare-react service and update container port mapping and database endpoint URL for pixshare-api service.
    - Add Spring Boot Actuator dependency to Maven POM for health and monitoring.
    - Configure actuator endpoints for web exposure to include health and info in Spring application properties.
    - Enable public access to actuator endpoints starting with "/actuator" in SecurityFilterChainConfig.

--- backend/src/main/java/com/pixshare/pixshareapi/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/pixshare/pixshareapi/security/SecurityFilterChainConfig.java
@@ -38,6 +38,9 @@ public class SecurityFilterChainConfig {
                         "/api/v1/auth/signup",
                         "/api/v1/auth/login")
                 .permitAll()
+                .requestMatchers(HttpMethod.GET,
+                        "/actuator/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
