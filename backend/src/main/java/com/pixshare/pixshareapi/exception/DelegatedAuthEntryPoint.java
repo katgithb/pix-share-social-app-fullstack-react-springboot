cc0202f99a45d7d6b9d3f260b1ec07da56f6a249
katgithb <90700219+katgithb@users.noreply.github.com>
2025-05-30 19:16:32 +0530

    Add custom AuthenticationEntryPoint for unauthorized exceptions
    - Implement AuthenticationEntryPoint to return JSON response with error details on authentication failure.
    - Configure ObjectMapper to handle Java 8 Date/Time API and disable date timestamps.
    - Include error message, status code, request URI, and timestamp in the response.

--- backend/src/main/java/com/pixshare/pixshareapi/exception/DelegatedAuthEntryPoint.java
+++ backend/src/main/java/com/pixshare/pixshareapi/exception/DelegatedAuthEntryPoint.java
@@ -1,27 +1,49 @@
 package com.pixshare.pixshareapi.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
 public class DelegatedAuthEntryPoint implements AuthenticationEntryPoint {
 
-    private final HandlerExceptionResolver handlerExceptionResolver;
+    private final ObjectMapper objectMapper;
 
-    public DelegatedAuthEntryPoint(HandlerExceptionResolver handlerExceptionResolver) {
-        this.handlerExceptionResolver = handlerExceptionResolver;
+    public DelegatedAuthEntryPoint() {
+        this.objectMapper = new ObjectMapper();
+
+        // Register module for Java 8 Date/Time API
+        this.objectMapper.registerModule(new JavaTimeModule());
+        // Prevent writing dates as timestamps
+        this.objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        handlerExceptionResolver.resolveException(request, response, null, authException);
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException e) throws IOException, ServletException {
+
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        response.setStatus(status.value());
+        response.setContentType("application/json");
+
+        ApiError apiError = new ApiError(request.getRequestURI(),
+                e.getMessage(),
+                "INSUFFICIENT_AUTHENTICATION",
+                status.value(),
+                LocalDateTime.now());
+
+        String jsonResponse = objectMapper.writeValueAsString(apiError);
+        response.getWriter().write(jsonResponse);
     }
-    
+
 }
