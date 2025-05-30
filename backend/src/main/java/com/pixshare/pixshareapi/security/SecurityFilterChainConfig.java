856094d618f9a25c57a12f0b34a760f29ca82f37
katgithb <90700219+katgithb@users.noreply.github.com>
2025-05-30 19:18:34 +0530

    Update security configuration for role-based authorization
    - Add role-based access control for admin endpoints with `RoleName.ADMIN`.
    - Refactor HTTP security configurations for cleaner syntax using lambdas.
    - Integrate `AccessDeniedHandler` for handling access denial scenarios explicitly.

--- backend/src/main/java/com/pixshare/pixshareapi/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/pixshare/pixshareapi/security/SecurityFilterChainConfig.java
@@ -1,6 +1,7 @@
 package com.pixshare.pixshareapi.security;
 
 import com.pixshare.pixshareapi.jwt.JWTAuthenticationFilter;
+import com.pixshare.pixshareapi.user.RoleName;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,9 +9,11 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -21,46 +24,43 @@ public class SecurityFilterChainConfig {
 
     private final AuthenticationEntryPoint authenticationEntryPoint;
 
+    private final AccessDeniedHandler accessDeniedHandler;
+
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityFilterChainConfig(AuthenticationProvider authenticationProvider, AuthenticationEntryPoint authenticationEntryPoint, JWTAuthenticationFilter jwtAuthenticationFilter) {
+    public SecurityFilterChainConfig(AuthenticationProvider authenticationProvider, AuthenticationEntryPoint authenticationEntryPoint, AccessDeniedHandler accessDeniedHandler, JWTAuthenticationFilter jwtAuthenticationFilter) {
         this.authenticationProvider = authenticationProvider;
         this.authenticationEntryPoint = authenticationEntryPoint;
+        this.accessDeniedHandler = accessDeniedHandler;
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
-                .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST,
-                        "/api/v1/auth/signup",
-                        "/api/v1/auth/login")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET,
-                        "/api/v1/users/public/**",
-                        "/api/v1/posts/public/**")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/v1/users/account/password/reset/**")
-                .permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/v1/users/account/password/reset/**")
-                .permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/v1/users/account/password/reset")
-                .permitAll()
-                .requestMatchers(HttpMethod.GET,
-                        "/actuator/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/v1/auth/signup",
+                                "/api/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/v1/users/public/**",
+                                "/api/v1/posts/public/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/users/account/password/reset/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/users/account/password/reset/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/users/account/password/reset").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                        .requestMatchers("/api/v1/admin/**").hasRole(RoleName.ADMIN.name())
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint);
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
+                );
 
         return http.build();
     }
